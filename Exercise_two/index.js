4ee9f619e2cc622068f6a2064ea9b877aaac0d98
cumar-dev <your_email@example.com>
2026-07-21 18:07:14 +0300

    exercise two

---
+++ Exercise_two/index.js
@@ -0,0 +1,23 @@
+"use strict";
+let names = ["omar", "cabdi", "axmed"];
+console.log(names);
+let garde = ["A+", "B+", "c+"];
+console.log(garde);
+let states = ["pending", "cancel", "done", "completed"];
+console.log(states);
+let products = ["phone", "laptop"];
+console.log(products);
+products.push("watch", "mouse");
+console.log(products);
+let localLocation = [
+    "Mogadishu",
+    2.0469,
+    45.3182,
+];
+console.log(localLocation);
+let usersInfo = {
+    "name": "omar",
+    "location": "mogadisho",
+    "phone": 612091248,
+};
+console.log(usersInfo);
---
+++ Exercise_two/index.ts
@@ -0,0 +1,27 @@
+let names = ["omar", "cabdi", "axmed"];
+console.log(names);
+let garde: Array<string> = ["A+", "B+", "c+"];
+console.log(garde);
+let states = ["pending", "cancel", "done", "completed"];
+console.log(states);
+
+let products = ["phone", "laptop"];
+console.log(products);
+products.push("watch", "mouse");
+console.log(products);
+
+let localLocation: [string, number, number] = [
+  "Mogadishu",
+  2.0469,
+  45.3182,
+];
+
+console.log(localLocation);
+
+let usersInfo: {} = {
+    "name": "omar",
+    "location": "mogadisho",
+    "phone": 612091248,
+}
+
+console.log(usersInfo);
